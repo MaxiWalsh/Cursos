616c3c1a64e70bb0c93866c56ce75d7e65a7852f
Maximiliano Walsh <91099967+MaxiWalsh@users.noreply.github.com>
2022-08-04 17:15:10 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,10 @@
+"use strict";
+var saludo = "HolaMundo desde TypeScript";
+saludo = "cambio";
+var numero;
+numero = 10;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+}
+saludar();
---
+++ funciones.ts
@@ -0,0 +1,15 @@
+let saludo = "HolaMundo desde TypeScript";
+
+saludo = "cambio";
+
+let numero:number;
+
+numero = 10;
+
+
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+}
+
+saludar();
